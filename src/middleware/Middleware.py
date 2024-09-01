e2602c38b58dd14429690e0d3848045516fc522c
Davi <131942060+zedark860@users.noreply.github.com>
2024-09-01 16:33:01 -0300

    Melhorias e organizando middleware para segurança

--- src/middleware/Middleware.py
+++ src/middleware/Middleware.py
@@ -1,5 +1,5 @@
 from flask import Flask, Response, jsonify, request
-from flask_jwt_extended import verify_jwt_in_request, get_jwt_identity, get_jwt
+from flask_jwt_extended import verify_jwt_in_request, get_jwt
 from werkzeug.exceptions import Unauthorized
 from validators.Validators import ValidatorsSchema
 
@@ -13,38 +13,60 @@ class Middleware:
         
     def __call__(self, environ, start_response) -> Response:
         with self.app.request_context(environ):
-            
-            protected_routes = ['/api/rota-teste-protegida']
-            
-            if request.path in protected_routes:
-                try:
-                    self.verificacoes_jwt()
-                except Unauthorized as error:
-                    res = jsonify({"msg": str(error)})
-                    res.status_code = 401
-                    return res(environ, start_response)
+            try:
+                if not self.is_public_route(request.path):
+                    self.verificacoes_rotas()
+            except Unauthorized as error:
+                res = jsonify({"msg": str(error)})
+                res.status_code = 401
+                return res(environ, start_response)
+            except Exception as error:
+                res = jsonify({"msg": str(error)})
+                res.status_code = 400
+                return res(environ, start_response)
         
         return self.wsgi_app(environ, start_response)
     
     
+    def verificacoes_rotas(self) -> None:
+        self.verificacoes_jwt()
+
+    
     def verificacoes_jwt(self) -> None:
-        auth_header: str = request.headers.get("Authorization")
-        if auth_header is None:
-            raise Unauthorized("Token de acesso não encontrado")
-                    
-        self.verify_jwt()
+        if not request.headers.get("Authorization"):
+            raise Unauthorized("Token de acesso ausente na requisição")
+        
+        verify_jwt_in_request()
                     
         if ValidatorsSchema.checar_se_token_esta_na_blacklist(get_jwt()):
             raise Unauthorized("Token de acesso revogado")
-                    
-        current_user: dict = get_jwt_identity()
-        # self.additional_checks(current_user)
-    
-
-    def verify_jwt(self) -> None:
-        verify_jwt_in_request()
         
+    
+    def is_public_route(self, path: str) -> bool:
+        return path in [
+            "/api/criar-usuario", 
+            "/api/login", 
+            "/api/requistar-troca-senha",
+            "/api/redefinir-senha",
+        ]
+   
 
-    def additional_checks(self, current_user: dict) -> None:
+    def verificar_admin_rotas(self, current_user: dict) -> None:
         if not current_user["admin"]:
             raise Unauthorized("Você não tem permissão para acessar esta rota")
+     
+        
+    # Não será necessário, pois o flask já verifica o conteúdo da requisição
+    # def verificacao_corpo_requisicao_json(self) -> None:
+    #     if not request.is_json or not request.content_type == "application/json":
+    #         raise Exception("Corpo da requisição deve ser JSON")
+        
+    
+    # Não será necessário, pois o flask já verifica o método http da requisição
+    # def verificacao_gerais(self) -> None:
+    #     if not request.method in ["GET", "POST", "PUT", "DELETE"]:
+    #         raise Exception("Metodo não permitido")
+        
+    #     # verificação se a rota existe 
+    #     if request.path not in self.app.url_map._rules_by_endpoint:
+    #         raise Exception("Rota não encontrada")
