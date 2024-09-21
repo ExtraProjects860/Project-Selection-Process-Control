751282b60c47ec4d2b1f1ebcecf21890bcb6b6c7
AlanaK2 <alanakarine27@gmail.com>
2024-09-21 13:29:12 -0300

    modal inscrição vaga

--- selection-process-control/src/components/avaliable-jobs/AvaliableJobs.jsx
+++ selection-process-control/src/components/avaliable-jobs/AvaliableJobs.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AvaliableJobs.css';
 import Pagination from '../pagination/Pagination';
+import JobApplicationModal from '../job-application-modal/JobApplicationModal';
 
 function AvaliableJobs() {
+
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     const availableJobs = [
         { id: 1, title: 'Vaga de Desenvolvedor Frontend', description: 'Desenvolva incríveis interfaces.' },
         { id: 2, title: 'Vaga de Designer UX/UI', description: 'Melhore a experiência do usuário.' },
         { id: 3, title: 'Vaga de Analista de Dados', description: 'Analise e transforme dados em insights.' },
       ];
+
+      const jobDetails = {
+        title: 'vaga tal',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet.'
+      };    
       
   return (
     <><div className="jobs-container">
@@ -20,7 +28,12 @@ function AvaliableJobs() {
             <h3>{job.title}</h3>
             <p>{job.description}</p>
             <hr></hr>
-            <button>Inscrever</button>
+            <button className='inscription-btn' onClick={() => setIsModalOpen(true)}>Inscrever</button>
+      <JobApplicationModal
+        jobDetails={jobDetails}
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      />
           </div>
         ))}
 
